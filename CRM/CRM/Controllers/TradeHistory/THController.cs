415bb9067c86bcbbf091019441829e7920712a50
vaytekhovski <arseniyvoytehovskiy@gmail.com>
2019-04-30 13:23:11 +0300

    change logic of paginal output of trade history

--- CRM/CRM/Controllers/TradeHistory/THController.cs
+++ CRM/CRM/Controllers/TradeHistory/THController.cs
@@ -11,6 +11,8 @@ namespace CRM.Controllers
     [Authorize]
     public class THController : Controller
     {
+        private static THService tHService;
+
         [HttpGet]
         public ActionResult TradeHistory()
         {
@@ -22,58 +24,32 @@ namespace CRM.Controllers
                 EndDate = DatesHelper.CurrentDateStr
             };
 
-            model = LoadTradeHistory(model);
+            tHService = new THService();
 
             return View(model);
         }
 
         [HttpPost]
-        public ActionResult TradeHistory(TradeHistoryFilterModel model)
+        public ActionResult TradeHistory(TradeHistoryFilterModel model, string PageButton = "0")
         {
-            model = LoadTradeHistory(model, 0);
+            model = LoadTradeHistory(model, Convert.ToInt32(PageButton));
 
             return View(model);
         }
 
-        public ActionResult PaginalOutPut(TradeHistoryFilterModel model, string PageButton)
-        {
-            model = GetTradeHistory(model, Convert.ToInt32(PageButton));
-
-            return View("TradeHistory", model);
-        }
-
-        private static THService tHService;
-
         private TradeHistoryFilterModel LoadTradeHistory(TradeHistoryFilterModel model, int PageNumber = 0)
         {
-            tHService = new THService();
-
             DateTime StartDate = DateTime.Parse(model.StartDate);
             DateTime EndDate = DateTime.Parse(model.EndDate).AddDays(1);
 
-            tHService.Load(model.Account, model.Coin, StartDate, EndDate);
-
-            model.Orders = tHService.AccountTradeHistories;
-
-            model.CountOfPages = tHService.CountOfPages;
-
-            model.Orders = model.Orders.Take(100).Skip(PageNumber * 100).ToList();
-
-            model.TotalProfit = tHService.TotalProfit;
-            model.TotalPercentProfit = tHService.TotalPercentProfit;
-
-            return model;
-        }
-
-        private TradeHistoryFilterModel GetTradeHistory(TradeHistoryFilterModel model, int PageNumber)
-        {
-            model.Orders = tHService.AccountTradeHistories
-                .Skip(PageNumber * 100)
-                .Take(100)
-                .ToList();
-
+            if (tHService.AccountTradeHistories.Count == 0)
+            {
+                tHService.Load(model.Account, model.Coin, StartDate, EndDate);
+            }
 
             model.CountOfPages = tHService.CountOfPages;
+
+            model.Orders = tHService.AccountTradeHistories.Skip((PageNumber - 1) * 100).Take(100).ToList();
 
             model.TotalProfit = tHService.TotalProfit;
             model.TotalPercentProfit = tHService.TotalPercentProfit;
