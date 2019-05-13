8c99ac96c26d22f9d9fe1daafe5a765c8e09e9cf
vaytekhovski <arseniyvoytehovskiy@gmail.com>
2019-05-13 14:35:32 +0300

    authorize fix on home page

--- CRM/CRM/Controllers/HomeController.cs
+++ CRM/CRM/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace CRM.Controllers
     public class HomeController : Controller
     {
         [HttpGet]
+        [Authorize]
         public ActionResult Index()
         {
             return View();
