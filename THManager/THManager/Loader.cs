ff179a1a1689d850644a850ae57f7cf49944c37a
arseniyvoytehovskiy <arseniyvoytehovskiy@Arseniys-MacBook-Air.local>
2019-05-26 16:11:08 +0300

    remove poloniex from th

--- THManager/THManager/Loader.cs
+++ THManager/THManager/Loader.cs
@@ -11,7 +11,7 @@ namespace THManager
         {
             using (MySqlContext context = new MySqlContext())
             {
-                 return context.Orders.Where(x => x.TimeEnded >= DateTime.Parse("2019-04-06")).OrderBy(x => x.TimeEnded).ToList();
+                 return context.Orders.Where(x => x.TimeEnded >= DateTime.Parse("2019-04-06") && x.AccountId != "bccd3ca1-0b5e-41ac-8233-3a35209912c7").OrderBy(x => x.TimeEnded).ToList();
             }
         }
     }
