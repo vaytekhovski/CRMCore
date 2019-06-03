4df6ff45d89b56ba296fec799e1b7e154e475af3
vaytekhovski <arseniyvoytehovskiy@gmail.com>
2019-06-03 13:30:14 +0300

    redline logs fix

--- THManager/THManager/Program.cs
+++ THManager/THManager/Program.cs
@@ -38,17 +38,17 @@ namespace THManager
             Console.WriteLine($"\n{DateTime.Now} |EveryTwoMinCalculate| Load Orders started");
             DateTime timeToLoad = Helper.FindTimeLastSell().AddHours(-3);
             var Orders = loader.LoadOrders(timeToLoad);
-            Console.WriteLine($"\n{DateTime.Now} |EveryTwoMinCalculate| Load Orders ended");
+            Console.WriteLine($"{DateTime.Now} |EveryTwoMinCalculate| Load Orders ended");
 
             Changer changer = new Changer();
             Console.WriteLine($"\n{DateTime.Now} |EveryTwoMinCalculate| Change Orders started");
             var TH = changer.ChangeOrdersBeforeCalculate(Orders);
-            Console.WriteLine($"\n{DateTime.Now} |EveryTwoMinCalculate| Change Orders ended");
+            Console.WriteLine($"{DateTime.Now} |EveryTwoMinCalculate| Change Orders ended");
 
             ProfitUpdater profitUpdater = new ProfitUpdater();
             Console.WriteLine($"\n{DateTime.Now} |EveryTwoMinCalculate| Update Profit and load to DB started");
             profitUpdater.UpdateProfit(TH);
-            Console.WriteLine($"\n{DateTime.Now} |EveryTwoMinCalculate| Update Profit and load to DB ended");
+            Console.WriteLine($"{DateTime.Now} |EveryTwoMinCalculate| Update Profit and load to DB ended");
             
             DrowLine();
         }
@@ -61,17 +61,17 @@ namespace THManager
             Console.WriteLine($"\n{DateTime.Now} |DailyCalculate| Load Orders started");
             DateTime timeToLoad = new DateTime(2019, 04, 06);
             var Orders = loader.LoadOrders(timeToLoad);
-            Console.WriteLine($"\n{DateTime.Now} |DailyCalculate| Load Orders ended");
+            Console.WriteLine($"{DateTime.Now} |DailyCalculate| Load Orders ended");
 
             Changer changer = new Changer();
             Console.WriteLine($"\n{DateTime.Now} |DailyCalculate| Change Orders started");
             var TH = changer.ChangeOrdersBeforeCalculate(Orders, false);
-            Console.WriteLine($"\n{DateTime.Now} |DailyCalculate| Change Orders ended");
+            Console.WriteLine($"{DateTime.Now} |DailyCalculate| Change Orders ended");
 
             ProfitUpdater profitUpdater = new ProfitUpdater();
             Console.WriteLine($"\n{DateTime.Now} |DailyCalculate| Update Profit and load to DB started");
             profitUpdater.UpdateProfit(TH, false);
-            Console.WriteLine($"\n{DateTime.Now} |DailyCalculate| Update Profit and load to DB ended");
+            Console.WriteLine($"{DateTime.Now} |DailyCalculate| Update Profit and load to DB ended");
 
             DrowStars();
         }
