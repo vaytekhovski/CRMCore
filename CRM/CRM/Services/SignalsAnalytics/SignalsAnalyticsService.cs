185470b9e899ef72252d1d625528cbae6adfc902
vaytekhovski <arseniyvoytehovskiy@gmail.com>
2019-06-04 14:19:48 +0300

    fixed linq

--- CRM/CRM/Services/SignalsAnalytics/SignalsAnalyticsService.cs
+++ CRM/CRM/Services/SignalsAnalytics/SignalsAnalyticsService.cs
@@ -23,15 +23,16 @@ namespace CRM.Services.SignalsAnalytics
         {
             using (masterContext context = new masterContext())
             {
-                SignalsPrivates = context.SignalsPrivate.Where(x =>
-                    model.Nullable == "null" ? x.ErrorMessages == null : model.Nullable == "all" ? x.ErrorMessages != null : true &&
-                    x.Exchange == model.Exchange.ToLower() &&
-                    model.Coin != "all" ? x.Base == model.Coin : true &&
-                    x.SourceTime >= DateTime.Parse(model.StartDate) &&
-                    x.SourceTime <= DateTime.Parse(model.EndDate).AddDays(1) &&
-                    x.SourceTime.TimeOfDay >= TimeSpan.Parse(model.StartTime) &&
-                    x.SourceTime.TimeOfDay <= TimeSpan.Parse(model.EndTime))
-                    .OrderByDescending(x => x.SourceTime).ToList();
+                SignalsPrivates = context.SignalsPrivate
+                    .Where(x => model.Nullable == "null" ? x.ErrorMessages == null : model.Nullable == "all" ? x.ErrorMessages != null : true)
+                    .Where(x => x.Exchange == model.Exchange.ToLower())
+                    .Where(x => model.Coin != "all" ? x.Base == model.Coin : true)
+                    .Where(x => x.SourceTime >= DateTime.Parse(model.StartDate))
+                    .Where(x => x.SourceTime <= DateTime.Parse(model.EndDate).AddDays(1))
+                    .Where(x => x.SourceTime.TimeOfDay >= TimeSpan.Parse(model.StartTime))
+                    .Where(x => x.SourceTime.TimeOfDay <= TimeSpan.Parse(model.EndTime))
+                    .OrderByDescending(x => x.SourceTime)
+                    .ToList();
 
                 model.SignalsPrivates = SignalsPrivates;
 
@@ -46,15 +47,14 @@ namespace CRM.Services.SignalsAnalytics
             using (masterContext context = new masterContext())
             {
                 TradeHistoryDeltas = context.TradeHistoryDelta
-                    .Where(x =>
-                    model.Nullable == "null" ? (x.MaxLongDiff == null && x.MinLongDiff == null) : model.Nullable == "notnull" ? (x.MaxLongDiff != null && x.MinLongDiff != null) : true &&
-                    x.Exchange == model.Exchange.ToLower() &&
-                    model.Coin != "all" ? x.Base == model.Coin : true &&
-                    model.Situation != "all" ? x.Situation == model.Situation.ToLower() : true && 
-                    x.TimeFrom >= DateTime.Parse(model.StartDate) &&
-                    x.TimeTo <= DateTime.Parse(model.EndDate).AddDays(1) &&
-                    x.TimeFrom.TimeOfDay >= TimeSpan.Parse(model.StartTime) &&
-                    x.TimeTo.TimeOfDay <= TimeSpan.Parse(model.EndTime))
+                    .Where(x => model.Nullable == "null" ? (x.MaxLongDiff == null && x.MinLongDiff == null) : model.Nullable == "notnull" ? (x.MaxLongDiff != null && x.MinLongDiff != null) : true)
+                    .Where(x => x.Exchange == model.Exchange.ToLower())
+                    .Where(x => model.Coin != "all" ? x.Base == model.Coin : true)
+                    .Where(x => model.Situation != "all" ? x.Situation == model.Situation.ToLower() : true)
+                    .Where(x => x.TimeFrom >= DateTime.Parse(model.StartDate))
+                    .Where(x => x.TimeTo <= DateTime.Parse(model.EndDate).AddDays(1))
+                    .Where(x => x.TimeFrom.TimeOfDay >= TimeSpan.Parse(model.StartTime))
+                    .Where(x => x.TimeTo.TimeOfDay <= TimeSpan.Parse(model.EndTime))
                     .OrderByDescending(x => x.TimeFrom)
                     .ToList();
 
