c0cce17bfa759c08b77b1440e89bd5d051718c09
vaytekhovski <arseniyvoytehovskiy@gmail.com>
2019-07-05 13:02:36 +0300

    pagination partial view

--- CRM/CRM/Services/Pagination/PaginationService.cs
+++ CRM/CRM/Services/Pagination/PaginationService.cs
@@ -17,7 +17,7 @@ namespace CRM.Services.Pagination
 
         public PaginationModel GetPaginationModel(int currentPage, int listCount, int pageSize = 100)
         {
-            paginationModel.CountOfPages = (int)Math.Ceiling((decimal)((double)listCount / 100));
+            paginationModel.CountOfPages = (int)Math.Ceiling((decimal)((double)listCount / pageSize));
             paginationModel.FirstVisiblePage = currentPage - 5 >= 1 ? currentPage - 3 : 1; //TODO: move to pagination control (partial view)
             paginationModel.LastVisiblePage = currentPage + 5 <= paginationModel.CountOfPages ? currentPage + 5 : paginationModel.CountOfPages;
 
