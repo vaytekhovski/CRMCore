4f08b229538414fcee839da40c0218ea8487bbd6
pilipushko <andrey98714@gmail.com>
2019-07-24 14:57:36 +0300

    fixed to top navbar

---
+++ CRM/CRM/Jobs/DailyTrigger.cs
@@ -0,0 +1,18 @@
+﻿using CRM.Services;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Jobs
+{
+    public class DailyTrigger : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            DailyTriggerService dailyTrigger = new DailyTriggerService();
+            
+        }
+    }
+}
