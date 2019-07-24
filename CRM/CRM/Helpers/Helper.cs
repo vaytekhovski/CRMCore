47b703bd1abfccd1fbf976ca2c43868e64bf9e35
vaytekhovski <arseniyvoytehovskiy@gmail.com>
2019-07-24 14:41:45 +0300

    changed "SIGN UP" btn in mobile version to "SIGN IN" & First date in filters changed to month ago from now

--- CRM/CRM/Helpers/Helper.cs
+++ CRM/CRM/Helpers/Helper.cs
@@ -10,7 +10,7 @@ namespace CRM.Helpers
 {
     public static class DatesHelper
     {
-        public static DateTime MinDate = new DateTime(2019, 4, 5, 0, 0, 0);
+        public static DateTime MinDate = DateTime.Now.AddMonths(-1);
 
         public static string MinDateTimeStr = MinDate.ToString("yyyy-MM-ddTHH:mm");
         public static string MinDateStr = MinDate.ToString("yyyy-MM-dd");
