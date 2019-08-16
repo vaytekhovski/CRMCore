34e70779ac436f6c29680baef862bb2d33c3daa2
arseniyvoytehovskiy <arseniyvoytehovskiy@Arseniys-MacBook-Air.local>
2019-08-16 12:22:43 +0300

    regular calculating changed to daily

--- CRM/Jobs/Program.cs
+++ CRM/Jobs/Program.cs
@@ -51,24 +51,22 @@ namespace QuartzSampleApp
                 ITrigger everyTwoMinTrigger = TriggerBuilder.Create()
                    .WithIdentity("trigger1", "group1")
                    .StartNow()
-                   .WithSchedule(CronScheduleBuilder.CronSchedule("0 1/2 * ? * *"))
+                   .WithSchedule(CronScheduleBuilder.CronSchedule("0 1/2 * * *"))
                    .Build();
 
                 ITrigger dailyTrigger = TriggerBuilder.Create()
                     .WithIdentity("trigger2", "group2")
                     .StartNow()
-                    .WithSchedule(CronScheduleBuilder.CronSchedule("0 1/2 ? * *"))
+                    .WithSchedule(CronScheduleBuilder.CronSchedule("0 01 15 * *"))
                     .Build();
 
                 ITrigger APITrigger = TriggerBuilder.Create()
                     .WithIdentity("trigger3", "group3")
                     .StartNow()
-                    .WithSchedule(CronScheduleBuilder.CronSchedule("25 56 13 ? * *"))
+                    .WithSchedule(CronScheduleBuilder.CronSchedule("25 56 13 * *"))
                     .Build();
 
-
-
-                //await scheduler.ScheduleJob(everyTwoMinLoading, everyTwoMinTrigger);
+                await scheduler.ScheduleJob(everyTwoMinLoading, everyTwoMinTrigger);
                 await scheduler.ScheduleJob(dailyLoading, dailyTrigger);
                 //await scheduler.ScheduleJob(APILoading, APITrigger);
 
