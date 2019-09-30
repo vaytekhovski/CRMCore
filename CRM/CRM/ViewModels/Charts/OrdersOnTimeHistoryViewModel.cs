2abc3f4eecf23631827289502ead67d3073e2e97
pilipushko <andrey98714@gmail.com>
2019-09-30 15:55:35 +0300

    ViewModel and View for OrdersOnTimeHistory

---
+++ CRM/CRM/ViewModels/Charts/OrdersOnTimeHistoryViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.ViewModels.Charts
+{
+    public class OrdersOnTimeHistoryViewModel
+    {
+        public string PageName { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+
+        public string Exchange { get; set; }
+        public string Base { get; set; }
+
+        public List<long> Dates { get; set; }
+        public List<string> RSIValues { get; set; }
+
+
+        public OrdersOnTimeHistoryViewModel()
+        {
+            RSIValues = new List<string>();
+            Dates = new List<long>();
+        }
+    }
+}
