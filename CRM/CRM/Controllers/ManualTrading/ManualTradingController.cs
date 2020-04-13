49a5f749f622489d067aa24739a8284056f08e70
vaytekhovski <arseniyvoytehovskiy@gmail.com>
2020-04-13 15:44:26 +0300

    Manual Trading

---
+++ CRM/CRM/Controllers/ManualTrading/ManualTradingController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRM.Helpers;
+using CRM.ViewModels.ManualTrading;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.Controllers.ManualTrading
+{
+    public class ManualTradingController : Controller
+    {
+        public IActionResult Trade()
+        {
+            ManualTradingModel ViewModel = new ManualTradingModel();
+
+            ViewModel.Account = "First";
+            ViewModel.Coin = "BTC";
+            ViewModel.Unit = new Unit
+            {
+                CountOfUnits5m = 5,
+                CountOfUnits15m = 14,
+                CountOfUnits30m = 27,
+                CountOfUnits1h = 29,
+                CountOfUnits3h = 78
+            };
+
+            SeparateHelper.Separator.NumberDecimalSeparator = ".";
+
+
+            ViewModel.Unit.PercentOfUnits5m = Math.Round(ViewModel.Unit.CountOfUnits5m / 5 * 100, 0).ToString(SeparateHelper.Separator);
+            ViewModel.Unit.PercentOfUnits15m = Math.Round(ViewModel.Unit.CountOfUnits15m / 15 * 100, 0).ToString(SeparateHelper.Separator);
+            ViewModel.Unit.PercentOfUnits30m = Math.Round(ViewModel.Unit.CountOfUnits30m / 30 * 100, 0).ToString(SeparateHelper.Separator);
+            ViewModel.Unit.PercentOfUnits1h = Math.Round(ViewModel.Unit.CountOfUnits1h / 60 * 100, 0).ToString(SeparateHelper.Separator);
+            ViewModel.Unit.PercentOfUnits3h = Math.Round(ViewModel.Unit.CountOfUnits3h / 180 * 100, 0).ToString(SeparateHelper.Separator);
+
+            
+
+            ViewModel.Units = new List<Unit>();
+
+            return View(ViewModel);
+        }
+    }
+}
---
+++ CRM/CRM/ViewModels/ManualTrading/ManualTradingModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.ViewModels.ManualTrading
+{
+    public class ManualTradingModel
+    {
+        public string Account { get; set; }
+        public string Coin { get; set; }
+        public Unit Unit { get; set; }
+        public List<Unit> Units { get; set; }
+
+    }
+
+    public class Unit
+    {
+        public DateTime Time { get; set; }
+        public double CountOfUnits3h { get; set; }
+        public string PercentOfUnits3h { get; set; }
+        public double CountOfUnits1h { get; set; }
+        public string PercentOfUnits1h { get; set; }
+        public double CountOfUnits30m { get; set; }
+        public string PercentOfUnits30m { get; set; }
+        public double CountOfUnits15m { get; set; }
+        public string PercentOfUnits15m { get; set; }
+        public double CountOfUnits5m { get; set; }
+        public string PercentOfUnits5m { get; set; }
+    }
+}
