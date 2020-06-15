9f11e24937db575e215f711d20ee7ef2a1363bfd
vaytekhovski <arseniyvoytehovskiy@gmail.com>
2020-06-15 15:28:02 +0300

    some changes

---
+++ CRM/CRM/ViewModels/Charts/SignalsModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.ViewModels.Charts
+{
+    public class SignalsModel
+    {
+        public SignalsModel()
+        {
+            Dates = new List<long>();
+            Values = new List<string>();
+        }
+
+        public string PageName { get; set; }
+        public string Account { get; set; }
+        public string Coin { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+
+        public List<long> Dates { get; set; }
+        public List<string> Values { get; set; }
+    }
+}
